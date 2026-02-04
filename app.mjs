a5727995c18c27800c5ffc7c0be7f89c79f179f3
Pong <vx7vx8vx9@gmail.com>
2026-02-04 13:52:57 +0700

    feat:Add answer API routes and enhance question deletion logic

--- app.mjs
+++ app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import questionApi from "./routes/questionApi.mjs";
+import answerApi, { questionAnswersRouter } from "./routes/answerApi.mjs";
 
 const app = express();
 const port = 4000;
@@ -7,6 +8,8 @@ const port = 4000;
 app.use(express.json());
 
 app.use("/questions", questionApi);
+app.use("/questions", questionAnswersRouter);
+app.use("/answers", answerApi);
 
 
 
